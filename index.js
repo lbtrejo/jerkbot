c53147d401608cb9602409ffb23aaeae66a15dc4
Levi Trejo <levi.b.trejo@gmail.com>
2020-11-11 16:09:08 -0600

    debugging bot auth

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 
 dotenv.config();
+
+const bot = new JerkBot({
+    token: `${process.env.BOT_TOKEN}`,
+    name: 'jerkbot9421'
+})
+
+bot.on('start', () => {
+    const params = {
+        icon_emoji: ':robot_face:'
+    }
+
+    bot.postMessageToChannel(
+        'random',
+        'Get inspired while working with @inspirenuggets',
+        params
+    );
+})
+
